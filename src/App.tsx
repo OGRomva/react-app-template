4032f68a93ee3a5ea9a42f3d234fa6be286a1020
Vadim <romazanovvadim2003@gmail.com>
2024-12-15 22:28:29 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+function App() { 
+  return (
+    <div className={styles.App}>
+      <div className="hui">huynia</div>
+    </div>
+  );
+}
+
+export default App;
