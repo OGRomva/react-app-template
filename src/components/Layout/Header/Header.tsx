80de4d01be29f8aac57b117bc0b228b944b2b8b9
Vadim <romazanovvadim2003@gmail.com>
2024-12-27 20:37:07 +0300

    fix: Header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -2,7 +2,7 @@ import styles from './Header.module.scss';
 
 const Header = () => {
     return <div className={styles.Header}>
-        <h1 className={styles.H1}></h1>
+        <h1 className={styles.H1}>Заголовок</h1>
     </div>
 }
 
